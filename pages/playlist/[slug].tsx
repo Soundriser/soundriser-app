d48a7a0012c6016644792d268ecebca8ae9d3201
Matteo Collina <collinamatteo@gmail.com>
2023-12-09 17:28:18 +0100

    Tolto hotjar e messo smartlook

--- pages/playlist/[slug].tsx
+++ pages/playlist/[slug].tsx
@@ -125,17 +125,16 @@ const Playlist = (props: { playlist: Playlist; data: any, slug: any }) => {
       <script>
         {
           `
-          (function(h,o,t,j,a,r){
-            h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-            h._hjSettings={hjid:3753807,hjsv:6};
-            a=o.getElementsByTagName('head')[0];
-            r=o.createElement('script');r.async=1;
-            r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-            a.appendChild(r);
-        })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+          window.smartlook||(function(d) {
+            var o=smartlook=function(){ o.api.push(arguments)},h=d.getElementsByTagName('head')[0];
+            var c=d.createElement('script');o.api=new Array();c.async=true;c.type='text/javascript';
+            c.charset='utf-8';c.src='https://web-sdk.smartlook.com/recorder.js';h.appendChild(c);
+            })(document);
+            smartlook('init', 'd2e25cca4d10481315480e8194e4da691428cdab', { region: 'eu' });
           `
         }
       </script>
+
       <script id="cookieyes" type="text/javascript" src="https://cdn-cookieyes.com/client_data/e1fdd1c41235629d0f76a892/script.js"></script>       
 
       </Head>
